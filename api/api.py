a5d54501d0d590f4777b559623f465de702816d7
GrowingSky <63eric36@gmail.com>
2025-09-12 10:41:38 -0300

    Add CORS middleware configuration to API; define ports and hosts for improved cross-origin requests

--- api/api.py
+++ api/api.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.bdgd import region_routes, substation_routes, trhv_routes
 
 api_prefix = '/api'
 bdgd_prefix = '/'.join([api_prefix, 'bdgd'])
 
 app = FastAPI()
+
+ports = [
+    8000
+]
+
+hosts = [
+    '172.25.0.232'
+]
+
+origins = [f'http://{host}:{port}' for host in hosts for port in ports]
+
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=origins,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(region_routes.router, prefix=bdgd_prefix)
 app.include_router(substation_routes.router, prefix=bdgd_prefix)
