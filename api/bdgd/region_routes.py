4df88626a3dbc7ac7d5fa0ccb868a31d02dd2bd6
GrowingSky <63eric36@gmail.com>
2025-09-17 10:41:37 -0300

    Add simplify_tolerance parameter to region query methods for enhanced geometry simplification control

--- api/bdgd/region_routes.py
+++ api/bdgd/region_routes.py
@@ -14,6 +14,7 @@ def get_region(
     dist: str | None = Query(None, description="Buscar pelo código da distribuidora."),
     poi: str | None = Query(None, description="Buscar a região que atua no Ponto de Interesse no formato 'latitude,longitude' (ex: '-25.55,-49.72')."),
     geometry: bool = Query(True, description="Opção para trazer ou não a geometria da região."),
+    simplify_tolerance: float = Query(0, description="Opção para aumentar a tolerancia da simplificação da regioão (mais tolerância, mais simples)"),
     limit: int | None = Query(None, description="Caso não seja utilizado nenhum filtro, o limite de registros para retornar."),
     offset: int | None = Query(None, description="Caso não seja utilizado nenhum filtro, por qual registro deve começar para retornar.")
 ):
@@ -38,21 +39,52 @@ def get_region(
 
     if len(unique_params) == 0:
         # Listagem paginada
-        return bdgd_manager.interface.get_all_regions(limit=limit, offset=offset, geometry=geometry)
+        return bdgd_manager.interface.get_all_regions(
+            limit,
+            offset,
+            True,
+            geometry,
+            simplify_tolerance
+        )
 
     param_name, param_value = unique_params[0]
 
     match param_name:
         case "id":
-            content = bdgd_manager.interface.get_region_by_id(int(param_value), geometry=geometry)
+            content = bdgd_manager.interface.get_region_by_id(
+                param_value,
+                True,
+                geometry,
+                simplify_tolerance
+            )
         case "bdgd_id":
-            content = bdgd_manager.interface.get_region_by_bdgd_id(str(param_value), geometry=geometry)
+            content = bdgd_manager.interface.get_region_by_bdgd_id(
+                param_value,
+                True,
+                geometry,
+                simplify_tolerance
+            )
         case "cod_id":
-            content = bdgd_manager.interface.get_region_by_cod_id(str(param_value), geometry=geometry)
+            content = bdgd_manager.interface.get_region_by_cod_id(
+                param_value,
+                True,
+                geometry,
+                simplify_tolerance
+            )
         case "name":
-            content = bdgd_manager.interface.get_region_by_bdgd_name(str(param_value), geometry=geometry)
+            content = bdgd_manager.interface.get_region_by_bdgd_name(
+                param_value,
+                True,
+                geometry,
+                simplify_tolerance
+            )
         case "dist":
-            content = bdgd_manager.interface.get_region_by_dist(str(param_value), geometry=geometry)
+            content = bdgd_manager.interface.get_region_by_dist(
+                param_value,
+                True,
+                geometry,
+                simplify_tolerance
+            )
         case "poi":
             try:
                 lat_str, lon_str = str(param_value).split(',')
@@ -64,7 +96,12 @@ def get_region(
                     f"{param_value} não é um par de coordenadas válido. Use 'longitude,latitude' (ex: '-49.72,-25.55'). Detalhe: {e}"
                 )
             poi_coord = (lon, lat)  # POINT(lon lat)
-            content = bdgd_manager.interface.get_region_by_poi(poi_coord, geometry=geometry)
+            content = bdgd_manager.interface.get_region_by_poi(
+                poi_coord,
+                True,
+                geometry,
+                simplify_tolerance
+            )
         case _:
             raise HTTPException(400, f"Parâmetro {param_name} inválido.")
 
--- app/bdgd_queries/region_queries.py
+++ app/bdgd_queries/region_queries.py
@@ -35,30 +35,67 @@ class RegionQueryMixin:
         limit: int | None = None,
         offset: int | None = None,
         mapped: bool = True,
-        geometry: bool = True
+        geometry: bool = True,
+        simplify_tolerance: float = 0
     ) -> Sequence[RowMapping] | Sequence[Row[Any]]:
         return self._select_many(self.region_table, None, limit, offset, mapped, geometry)  # type: ignore
 
     # Filtros
-    def get_region_by_id(self, id: int, mapped: bool = True, geometry: bool = True):
+    def get_region_by_id(
+            self,
+            id: int,
+            mapped: bool = True,
+            geometry: bool = True,
+            simplify_tolerance: float = 0
+        ):
         where = self.region_table.c.id == id
-        return self._select_one(self.region_table, where, mapped, geometry)  # type: ignore
+        return self._select_one(self.region_table, where, mapped, geometry, simplify_tolerance)  # type: ignore
 
-    def get_region_by_bdgd_id(self, bdgd_id: str, mapped: bool = True, geometry: bool = True):
+    def get_region_by_bdgd_id(
+        self,
+        bdgd_id: str,
+        mapped: bool = True,
+        geometry: bool = True,
+        simplify_tolerance: float = 0
+    ):
         where = self.region_table.c.bdgd_id == bdgd_id
-        return self._select_one(self.region_table, where, mapped, geometry)  # type: ignore
+        return self._select_one(self.region_table, where, mapped, geometry, simplify_tolerance)  # type: ignore
 
-    def get_region_by_cod_id(self, cod_id: str, mapped: bool = True, geometry: bool = True):
+    def get_region_by_cod_id(
+        self,
+        cod_id: str,
+        mapped: bool = True,
+        geometry: bool = True,
+        simplify_tolerance: float = 0
+    ):
         where = self.region_table.c.cod_id == cod_id
-        return self._select_one(self.region_table, where, mapped, geometry)  # type: ignore
+        return self._select_one(self.region_table, where, mapped, geometry, simplify_tolerance)  # type: ignore
 
-    def get_region_by_dist(self, dist: str | int, mapped: bool = True, geometry: bool = True):
+    def get_region_by_dist(
+        self,
+        dist: str | int,
+        mapped: bool = True,
+        geometry: bool = True,
+        simplify_tolerance: float = 0
+    ):
         where = self.region_table.c.dist == str(dist)
-        return self._select_one(self.region_table, where, mapped, geometry)  # type: ignore
+        return self._select_one(self.region_table, where, mapped, geometry, simplify_tolerance)  # type: ignore
 
-    def get_region_by_bdgd_name(self, bdgd_name: str, mapped: bool = True, geometry: bool = True):
+    def get_region_by_bdgd_name(
+        self,
+        bdgd_name: str,
+        mapped: bool = True,
+        geometry: bool = True,
+        simplify_tolerance: float = 0
+    ):
         where = self.region_table.c.bdgd_name.like(bdgd_name)
-        return self._select_one(self.region_table, where, mapped, geometry)  # type: ignore
+        return self._select_one(self.region_table, where, mapped, geometry, simplify_tolerance)  # type: ignore
 
-    def get_region_by_poi(self, poi: tuple[float, float], mapped: bool = True, geometry: bool = True):
-        return self._select_one_by_poi_within(self.region_table, poi, mapped, geometry) # type: ignore
+    def get_region_by_poi(
+        self,
+        poi: tuple[float, float],
+        mapped: bool = True,
+        geometry: bool = True,
+        simplify_tolerance: float = 0
+    ):
+        return self._select_one_by_poi_within(self.region_table, poi, mapped, geometry, simplify_tolerance) # type: ignore
